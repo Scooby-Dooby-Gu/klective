d3ee10488e982ee4139791418ae2f4c3e6aae61e
Valentina Gu <valentinagu@valentinas-mbp.mynetworksettings.com>
2025-05-07 14:54:01 -0400

    chore: add debug and migrations directories to gitignore

--- migrations/remove_reference_columns.sql
+++
@@ -1,5 +0,0 @@
--- Remove reference columns from timeline_events table
-ALTER TABLE timeline_events
-    DROP COLUMN IF EXISTS event_references,
-    DROP COLUMN IF EXISTS page_or_section_references,
-    DROP COLUMN IF EXISTS source_reference; 
